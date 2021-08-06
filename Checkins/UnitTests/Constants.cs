5407f02aa079a56ac03c5e616cbf16fdb40f355b
Tanveer727 <79864037+Tanveer727@users.noreply.github.com>
2021-08-07 00:57:48 +0530

    Add Test case logic

--- Checkins/UnitTests/Constants.cs
+++ Checkins/UnitTests/Constants.cs
@@ -8,8 +8,8 @@ namespace UnitTesting
 {
     public static class Constants
     {
-        public const string Baseurl = "http://localhost:58202/Default.aspx";
-        public const string LoginPage = "http://localhost:58202/log.aspx";
-        public const string RegPage = "http://localhost:58202/regi.aspx";
+        public const string Baseurl = "http://localhost:55098/Default.aspx";
+        public const string LoginPage = "http://localhost:55098/log.aspx";
+        public const string RegPage = "http://localhost:55098/regi.aspx";
     }
 }
